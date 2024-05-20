c2bde1cedbe1f3f2e8e6a13b81ae21925def69ea
nicolas.woerner <nicolaswoerner@MBPvonNicolas2.fritz.box>
2024-05-20 13:56:05 +0200

    use default

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -16,7 +16,7 @@ else
 fi
 
 source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
-
+docker buidix use default
 docker build --platform $PLATFORM build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ghcr.io/commaai/openpilot-base:latest -t $REMOTE_TAG -t $LOCAL_TAG -o docker -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
 
 docker images
