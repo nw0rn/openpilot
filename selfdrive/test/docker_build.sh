190d4c1f85692f4c5081127bfff87248974037c8
nicolas.woerner <nicolaswoerner@MBPvonNicolas2.fritz.box>
2024-05-20 18:10:21 +0200

    fix

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -17,7 +17,7 @@ fi
 
 source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 
-docker build --build-arg BUILDKIT_INLINE_CACHE=1 --platform $PLATFORM --cache-from type=registry,ref=ghcr.io/nw0rn/openpilot/openpilot-base --cache-to type=inline -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
+docker build --build-arg BUILDKIT_INLINE_CACHE=1 --platform $PLATFORM --cache-from type=registry,ref=ghcr.io/nw0rn/openpilot/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
 
 docker images
 docker ps
